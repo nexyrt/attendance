6779c3bdb0717d95d52d3899edab489c23f2c3b7
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2025-09-19 04:19:14 +0800

    logo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="my-6 flex items-center justify-center">
-        <img src="{{ asset('/assets/images/kisantra-logo.png') }}" />
+        <img class="w-6" src="{{ asset('/assets/images/kisantra-logo.png') }}" />
     </div>
 
     <form method="POST" action="{{ route('register') }}">
